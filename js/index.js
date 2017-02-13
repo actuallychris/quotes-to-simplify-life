420a7ccab36dd3365e2b7757b386ae67128e7687
Christopher Smith <actuallychris@gmail.com>
2017-02-12 22:34:45 -0700

    1st commit 12 feb 2017

--- js/index.js
+++ js/index.js
@@ -82,7 +82,21 @@ function genQuote() {
 }
 
 //quote array
-var quotes = ["Blank", "\"Dude, suckin' at something is the first step at being sorta good at something.\"<br>-  Jake <small><em>(Adventure Time)</em></small>", "\"Either I will find a way, or I will make one.\"<br> - Philip Sidney", "\"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.\"<br>- Thomas A. Edison", "\"You are never too old to set another goal or to dream a new dream.\"<br>- C.S Lewis", "\"If you can dream it, you can do it.\"<br>- Walt Disney", "\"Never give up, for that is just the place and time that the tide will turn.\"<br>- Harriet Beecher Stowe", "\"I know where I'm going and I know the truth, and I don't have to be what you want me to be. I'm free to be what I want.\"<br>- Muhammad Ali", "\"If you always put limit on everything you do, physical or anything else. It will spread into your work and into your life. There are no limits. There are only plateaus, and you must not stay there, you must go beyond them.\"<br>- Bruce Lee",];
+var quotes = ["Blank",
+              "A Man Paints with his mind not with his hands <br> -Michaelangelo";
+              "The Atonement of Jesus Christ makes the Savior’s invitation to “be ye therefore perfect, even as your Father which is in heaven is perfect” perfectly possible rather than frustratingly out of reach. <br> - Allen D. Haynie";
+              "He Provided the way by his death. He showed us the way by his life.<br> - Kaela Lloyd";
+              "That which holds the attention determines the action	William James";
+              "The greatest discovery of my age is that men can change their circumstances by changing the attitude of their minds.<br> - William James";
+
+
+
+
+
+
+
+
+             ];
 
 //date
 function startDate() {
